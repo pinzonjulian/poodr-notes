ec5929a8206c787578cefedb40f4616243250983
Julián Pinzón <julianpinzoneslava@gmail.com>
2019-03-20 07:24:03 -0500

    Update 1-inject-dependencies.rb

--- code-examples/chapter-3/1-inject-dependencies.rb
+++ code-examples/chapter-3/1-inject-dependencies.rb
@@ -1,4 +1,24 @@
+# Bad
 class Gear
+  attr_deader :chainring, :cog, :rim, :tire
+  
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+    
+  end
+  
+  def gear_ratio
+    # Gear needs to know that the Wheel class exists deep inside the gear_ratio method.
+    radio * Wheel.new(rim, tire).diameter
+  end
+end
+
+# Good
+class Gear
+  # using wheel as an argument cut the dependency   
   attr_deader :chainring, :cog, :wheel
   
   def initialize(chainring, cog, wheel)
